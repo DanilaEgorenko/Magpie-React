0f55ee441613ba6c2f526a289024120d03db4d3b
Danila Egorenko <energystyle16@gmail.com>
2022-09-21 20:54:15 +0300

    changed config location

--- src/services/accountInfo.ts
+++ src/services/accountInfo.ts
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebaseConfig from './firebaseConfig';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBY7x4GhWxB9-d2dDVejFnCaJWIXoQOCko",
-    authDomain: "chat-2454e.firebaseapp.com",
-    databaseURL: "https://chat-2454e.firebaseio.com",
-    projectId: "chat-2454e",
-    storageBucket: "chat-2454e.appspot.com",
-    messagingSenderId: "96916330062",
-    appId: "1:96916330062:web:7274907c620e0ba9e1d51f",
-}
 const app = initializeApp(firebaseConfig)
 const provider = new GoogleAuthProvider()
 const auth = getAuth(app)
